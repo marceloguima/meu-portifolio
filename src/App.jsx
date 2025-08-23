74a981993b3e1855fc8d79cb3ae863c2ef3b4c88
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-08-23 19:36:47 -0400

    importa subtitulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Header from "./componentes/Header";
 import Hero from "./componentes/Hero";
+import Subtitulo from "./componentes/Subtitulo";
+
 import MeusProjetos from "./componentes/MeusProjetos";
 
 function App() {
@@ -8,7 +10,9 @@ function App() {
         <div className="container">
             <Header />
             <Hero />
+            <Subtitulo>Meus projetos</Subtitulo>
             <MeusProjetos />
+            <Subtitulo>Tecnologias</Subtitulo>
         </div>
     );
 }
